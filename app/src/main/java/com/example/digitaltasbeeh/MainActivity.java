47da8ea08fe944ede6e24a5cdfbabf7efe1cc3c2
srabonx <srabon9012@gmail.com>
2024-06-30 14:00:05 +0600

    "First commit"

---
+++ app/src/main/java/com/example/digitaltasbeeh/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.digitaltasbeeh;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public class ButtonActivity implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+            Button b1 = (Button)v;
+
+            b1.setText("Clicked");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button b = findViewById(R.id.clickButton);
+        b.setOnClickListener(new ButtonActivity());
+    }
+}
